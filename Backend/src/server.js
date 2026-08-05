ac77a9ca2a62cc9c9db981984cda90f44440908c
Tanish Sahu <tanish23sahu@gmail.com>
2026-08-05 11:46:28 +0530

    New Change for Deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,22 +1,31 @@
 import express, { json, urlencoded } from "express" 
-import notesRoutes from "./Routes/notesRoutes.js"
-import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
-import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 
+import { connectDB } from "./config/db.js";
+import notesRoutes from "./Routes/notesRoutes.js"
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
+
 console.log(process.env.PORT);
 console.log(process.env.MONGO_URL);
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+const __dirname = path.resolve()
 
 
 //Middleware
-app.use(cors({
-    origin: "http://localhost:5173"
-}))  //The cors middle ware must be before because we are sending response in rate limiting so we need cors in it
-app.use(express.json());
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173"
+    }))  //The cors middle ware must be before because we are sending response in rate limiting so we need cors in it
+}
+
+
+    app.use(express.json());
 app.use(rateLimiter)
 
 
@@ -29,7 +38,14 @@ app.use(rateLimiter)
     
     
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
     
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"))
+    })    
+}
 connectDB().then(() => { // We are first connecting to database then only starting the app
     app.listen(PORT, () => {
         console.log("Server started on port:",PORT)
